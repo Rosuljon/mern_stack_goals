e0f6eeebf2efebee677e6eb085c83c1856407b51
Rosuljon <ikramjanov97@mail.ru>
2022-11-09 16:28:14 +0900

    connected to mongoDB

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,20 +1,44 @@
 import asyncHandler from 'express-async-handler';
+import Goal from '../modals/goalModal.js'
+
 
 const getGoals = asyncHandler( async (req, res) =>{
-    res.status(200).json({message: 'Goals'});
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 })
+
+
 const postGoals = asyncHandler( async (req, res) =>{
     if(!req.body.text){
         res.status(400)
         throw new Error('please enter a title')
     }
-    res.status(200).json({message: 'Set Goals'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 });
+
+
 const putGoals = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: `update goals ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400);
+        throw new Error('goal not found');
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new : true})
+    res.status(200).json(updatedGoal);
 });
+
+
 const deleteGoals = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: `delete goals ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('goal not found')
+    }
+    await goal.remove()
+    res.status(200).json({id : req.params.id});
 });
 
 export {getGoals, postGoals, putGoals, deleteGoals};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
+import colors from 'colors';
 const port = process.env.PORT || 5000;
 import router from './routes/goalRoutes.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
+connectDB();
 
 const app = express();
 app.use(express.json());
