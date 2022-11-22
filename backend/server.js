02fe2a7252703a2eece98756a9a9346d614dc23e
Ronny <ikramjanov97@mail.ru>
2022-11-22 15:49:28 +0900

    ready to deployment backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
-import dotenv from 'dotenv/config';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import colors from 'colors';
 const port = process.env.PORT || 5000;
 import routerGoal from './routes/goalRoutes.js';
 import routerUser from './routes/userRoutes.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-
+dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+app.use(cors());
 app.use('/api/goals', routerGoal)
 app.use('/api/users', routerUser);
 app.use(errorHandler);
